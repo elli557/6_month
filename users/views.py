8503935b27988b7dca9671eb62f97604122965ed
emwauqz <emwauqz@gmail.com>
2025-07-16 01:32:36 +0600

    hw1

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from users.serializers import CustomTokenObtainSerializer
 from users.models import CustomUser
@@ -17,29 +17,6 @@ from .serializers import (
 from .models import ConfirmationCode
 import random
 import string
-
-
-class AuthorizationAPIView(APIView):
-    def post(self, request):
-        serializer = AuthValidateSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        user = authenticate(**serializer.validated_data)
-
-        if user:
-            if not user.is_active:
-                return Response(
-                    status=status.HTTP_401_UNAUTHORIZED,
-                    data={'error': 'User account is not activated yet!'}
-                )
-
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response(data={'key': token.key})
-
-        return Response(
-            status=status.HTTP_401_UNAUTHORIZED,
-            data={'error': 'User credentials are wrong!'}
-        )
 
 
 class RegistrationAPIView(CreateAPIView):
@@ -60,7 +37,6 @@ class RegistrationAPIView(CreateAPIView):
             )
 
             code = ''.join(random.choices(string.digits, k=6))
-
             confirmation_code = ConfirmationCode.objects.create(
                 user=user,
                 code=code
@@ -75,15 +51,47 @@ class RegistrationAPIView(CreateAPIView):
         )
 
 
-class ConfirmUserAPIView(APIView):
+class AuthorizationAPIView(GenericAPIView):  
+    serializer_class = AuthValidateSerializer
+
     def post(self, request):
-        serializer = ConfirmationSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data) 
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data={'error': 'неверный email или пароль'}
+            )
+        if not user.check_password(password):
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data={'error': 'неверный email или пароль'}
+            )
+        if not user.is_active:
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data={'error': 'аккаунт не подтверждён'}
+            )
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response(data={'key': token.key})
+
+
+class ConfirmUserAPIView(GenericAPIView): 
+    serializer_class = ConfirmationSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data) 
         serializer.is_valid(raise_exception=True)
 
         user_id = serializer.validated_data['user_id']
 
         with transaction.atomic():
-            user = User.objects.get(id=user_id)
+            user = CustomUser.objects.get(id=user_id)
             user.is_active = True
             user.save()
 
@@ -98,7 +106,6 @@ class ConfirmUserAPIView(APIView):
                 'key': token.key
             }
         )
-        
         
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainSerializer
