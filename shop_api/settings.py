c675d0b09dd781a0962c6e1f7033c8cc600c8025
emwauqz <emwauqz@gmail.com>
2025-07-28 02:13:11 +0600

    HW_6

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config, Csv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "JGFCJHGCKHGCKJJjhgdjhgc"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('DEBUG') == 'on' else False
+SECRET_KEY = config("SECRET")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
--- users/tasks.py
+++ users/tasks.py
@@ -1,12 +1,13 @@
 from celery import shared_task
 import time
 from django.core.mail import send_mail
+from django.conf import settings
 
 @shared_task
 def send_otp_email(user_email, code):
     subject = "Ваш код подтверждения"
     message = f"Ваш код подтверждения: {code}"
-    from_email = "your_email@gmail.com"
+    from_email = settings.EMAIL_HOST_USER
 
     send_mail(
         subject,
